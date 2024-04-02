b80d7ddf78be694b484c28d644745384b6338cc8
EDUARDO DIAS DA SILVA <eduardo.dsilva17@senacsp.edu.br>
2024-04-02 15:41:21 -0300

    dia 2

--- js/8/script.js
+++ js/8/script.js
@@ -1,7 +1,41 @@
-
 let n= 1
-while (n < 101){
+while (n <= 100){
     console.log(`${n}`)
     n++
 }
 
+// let n= 100
+// while (n > 0){
+//     console.log(`${n}`)
+//     n--
+// }
+
+// let n = 1
+// do {
+//     console.log(`${n}`)
+//     n += 5  
+// }while(n <= 100)
+
+// let n= 0
+// while (n <= 100){
+//     console.log(n)
+//     n += 5
+// }
+
+// let n= 100
+// while (n > 0){
+//     console.log(n)
+//     n -= 5
+// }
+
+// console.log("numero de 1 a 100:")
+
+// for(let i  = 1; i <= 100; i++){
+//     console.log(i)
+// }
+
+// console.log("numero de 1 a 100:")
+
+// for(let i  = 100; i >= 0; --i){
+//     console.log(i)
+// }
